3c8bbf295238e0716098d43be6efc9c3b2f93df7
Evans <@github.com:E-vanson/Node_tutorial.git>
2023-10-31 22:44:56 +0300

    Third commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const {PORT, mongoUri} = require('./config')
 //const mongoUri = require('./config')
 const mongoose = require('mongoose')
 const Book = require('./models/book')
+const bookRouter = require('./routes/book')
+const cors = require('cors')
 
 
 const app = express();
@@ -10,56 +12,26 @@ const app = express();
 //add middleware to parse json resonse
 app.use(express.json())
 
+//routes
+app.use('/books', bookRouter)
+app.use('/books/:id', bookRouter)
+
+//cors Allows all origin with default of cors(*)
+app.use(cors())
+
+//Allows custom origins
+app.use(cors({
+    //only req with this origin will access the server
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-type']
+}))
+
 app.get('/', (req, res)=>{
     res.status(200).send("Welcome to the home page")
     
 })
-app.post('/books', async (req,res)=>{
-    try {
-        if(!req.body.title || !req.body.author || !req.body.publishYear){
-            res.status(400).send("Enter the details required")
-        }
-        const newBook = {
-            title: req.body.title,
-            author: req.body.author,
-            publishYear: req.body.publishYear
-        }
-        
-        const book = await Book.create(newBook)
-        //console.log(book)
-        res.status(201).json({book})
-    } catch (error) {
-        res.status(400).send(error.message)
-    }
-})
 
-//Route to get all the books
-app.get('/books', async (req, res)=>{
-    try {
-        const books = await Book.find({}) 
-        res.status(200).json({
-            count: books.length,
-            data:books})
-    } catch (error) {
-        console.log(error.message)
-        res.status(404).send("Resource not found")
-    }
-})
-
-//Route to get a single book
-app.get('/books/:id', async (req, res)=>{
-    try {
-        if(!req.params){
-            res.status(400).json({msg:'Bad request error'})
-        }
-        //destructure the id from the req
-        const {id} = req.params
-        const book = await Book.findById({_id:id})
-        res.status(200).json({data:book})
-    } catch (error) {
-        res.status(500).json({msg: error.message})
-    }
-})
 // console.log(mongoUri)
 // console.log(PORT)
 
