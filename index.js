02631326e9e603918ef9afb093cd8aa202de19ff
Evans <@github.com:E-vanson/Node_tutorial.git>
2023-11-08 23:43:06 +0300

    Fixed cors

--- index.js
+++ index.js
@@ -2,29 +2,39 @@ const express = require('express')
 const {PORT, mongoUri} = require('./config')
 //const mongoUri = require('./config')
 const mongoose = require('mongoose')
-const Book = require('./models/book')
+//const Book = require('./models/book')
 const bookRouter = require('./routes/book')
 const cors = require('cors')
-const helmet = require('helmet')
+//const helmet = require('helmet')
 
 
 
 const app = express();
-app.enable('trust proxy')
+//app.enable('trust proxy')
 //cors Allows all origin with default of cors(*)
 
 // app.options('*', cors())
 
-// app.use(helmet())
+ //app.use(helmet())
 
 //add middleware to parse json resonse
 app.use(express.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // You can specify a specific origin instead of '*'
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
+
+app.use(cors())
 
 //routes
 app.use('/books', bookRouter)
 app.use('/books/:id', bookRouter)
 
-app.use(cors())
+
 
 //Allows custom origins
 // app.use(cors({
--- models/book.js
+++ models/book.js
@@ -15,4 +15,4 @@ const bookSchema = mongoose.Schema({
     }
 },{timestamps: true})
 
-module.exports = mongoose.model('book', bookSchema)
+module.exports = mongoose.model('Book', bookSchema)
